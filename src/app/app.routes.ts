102a4fdd2edac377c064f3a2ee3722ced9857e46
gabrielsh06 <shgabriel06@gmail.com>
2026-08-24 18:01:21 -0500

    feat(app): PROJ-10 implementar layouts admin, public y configuracion de las rutas base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PublicLayout } from './layouts/public-layout/public-layout';
+import { AdminLayout } from './layouts/admin-layout/admin-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PublicLayout },
+  { path: 'admin', component: AdminLayout },
+  { path: '**', redirectTo: '' }
+];
