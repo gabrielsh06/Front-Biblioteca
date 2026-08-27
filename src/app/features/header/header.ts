7fc2481e05bb732bb0ea87653c1395cefbb0da28
Aldair-AS27 <aldairzero7@gmail.com>
2026-08-27 11:43:26 -0500

    PROJ-8: creacion de los componetesd del header (html, scss, ts)

--- src/app/features/header/header.ts
+++ src/app/features/header/header.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  imports: [],
   selector: 'app-header',
-  styleUrl: './header.scss',
-  templateUrl: './header.html',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
 })
-export class Header {}
+export class HeaderComponent {
+  // Aquí puedes agregar variables como el contador del carrito o el estado del usuario
+  cartItemCount: number = 0;
+  
+  onSearch(term: string) {
+    console.log('Buscando:', term);
+    // Lógica de búsqueda
+  }
+}
