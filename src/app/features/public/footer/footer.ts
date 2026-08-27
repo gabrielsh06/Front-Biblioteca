f456746e83b61805a7c900f60277c854c2013740
u23324789@utp.edu.pe <u23324789@utp.edu.pe>
2026-08-27 10:36:15 -0500

    Por completar footer

---
+++ src/app/features/public/footer/footer.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {CommunModule} from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css'
+})
+export class Footer {
+cargador=["youtube.com","facebook.com","linkedin.com","12"]
+
+
+}
