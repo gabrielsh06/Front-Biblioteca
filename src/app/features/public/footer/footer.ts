ee22c1575b14454f0c092d9586bf70b8d153d89c
RoymasE <u23324789@utp.edu.pe>
2026-08-27 22:39:04 -0500

    PROJ-12/ actualizacion del archivo ts de footer con nuevos datos en css

--- src/app/features/public/footer/footer.ts
+++ src/app/features/public/footer/footer.ts
@@ -1,15 +1,44 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {CommunModule} from '@angular/common';
+import {CommonModule} from '@angular/common';
+
+
 @Component({
   selector: 'footer',
-  imports: [RouterOutlet, CommunModule],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
 export class FooterClass {
-
-
-
-
+myj = {nombre:"Libreria purpure"};
+drw = {message:"*************"+
+             "\n**-*******-**"+
+             "\n*---*****---*"+
+             "\n-+++-----+++-"+
+             "\n-++00+++00++-"
+             "\n-++00+^+00++-"+
+             "\n-+++++=+++++-"
+             "\n-------------"};
+sumar(a:number, b:number): number{
+return (a+b);
 }
+
+getstring(a:number): string{
+return String(a);
+}
+
+  
+getnumber(a: string): number{
+return Number(a);
+}  
+  
+multiplicar(a:number, b:number):number{
+return (a*b);
+}
+  
+dividir(a:number, b:number):number{
+return (a/b);
+}  
+  
+}
+
