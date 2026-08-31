4591705774f2b0a3ec1da331e83ce43f6130b368
gabrielsh06 <shgabriel06@gmail.com>
2026-08-31 12:29:36 -0500

    refactor(footer): limpiar el ts

--- src/app/features/public/footer/footer.ts
+++ src/app/features/public/footer/footer.ts
@@ -1,45 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {CommonModule} from '@angular/common';
-
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'footer',
-  imports: [RouterOutlet, CommonModule],
+  selector: 'app-footer',
+  imports: [RouterModule],
   templateUrl: './footer.html',
-  styleUrl: './footer.css'
+  styleUrl: './footer.scss'
 })
-export class FooterClass {
-myj = {nombre:"Libreria purpure"};
-drw = {message:"*************"+
-             "\n**-*******-**"+
-             "\n*---*****---*"+
-             "\n-+++-----+++-"+
-             "\n-++00+++00++-"
-             "\n-++00+^+00++-"+
-             "\n-+++++=+++++-"+
-             "\n-------------"+
-	         "\n-------------"};
-sumar(a:number, b:number): number{
-return (a+b);
-}
-
-getstring(a:number): string{
-return String(a);
-}
-
-  
-getnumber(a: string): number{
-return Number(a);
-}  
-  
-multiplicar(a:number, b:number):number{
-return (a*b);
-}
-  
-dividir(a:number, b:number):number{
-return (a/b);
-}  
-  
-}
-
+export class FooterComponent {}
