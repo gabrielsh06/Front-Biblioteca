7adb097c4954a29287256ce3acb3fe499bfc434c
Aldair-AS27 <aldairzero7@gmail.com>
2026-09-04 13:04:54 -0500

    feat(header): implementacion del header con estilos de PROJ-10 y PROJ-15

---
+++ src/app/features/public/header/header.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [RouterModule],
+  templateUrl: './header.html',
+  styleUrl: './header.scss'
+})
+export class HeaderComponent {
+  onSearch(term: string) {
+    console.log('Buscando:', term);
+  }
+}
--- src/app/layouts/public-layout/public-layout.ts
+++ src/app/layouts/public-layout/public-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from '../../features/public/footer/footer';
+import { HeaderComponent } from '../../features/public/header/header';
 
 @Component({
   selector: 'app-public-layout',
-  imports: [RouterModule, FooterComponent],
+  imports: [RouterModule, FooterComponent, HeaderComponent],
   templateUrl: './public-layout.html',
   styleUrl: './public-layout.scss',
 })
