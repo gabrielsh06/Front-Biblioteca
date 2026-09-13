2467ed33f5d77b633a2db86aee817bc3b1b7949c
gabrielsh06 <shgabriel06@gmail.com>
2026-09-13 17:22:00 -0500

    feat(book-detail): agregar componentes iniciales

---
+++ src/app/features/public/pages/book-detail/book-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-book-detail',
+    imports: [],
+    templateUrl: './book-detail.html',
+    styleUrl: './book-detail.scss',
+})
+export class BookDetail {}
