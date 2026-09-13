9af292737271839eec3a1c030e522f7e78c60d0d
gabrielsh06 <shgabriel06@gmail.com>
2026-09-13 15:11:38 -0500

    feat(book-card): implementar la tarjeta de libro y listo para recibir datos

--- src/app/shared/public/book-card/book-card.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-book-card',
-    imports: [],
-    templateUrl: './book-card.html',
-    styleUrl: './book-card.scss',
-})
-export class BookCard {}
