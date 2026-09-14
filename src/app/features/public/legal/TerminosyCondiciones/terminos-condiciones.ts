31f2b20d28a303983ebc0d27affa14c87458b1a6
RoymasE <u23324789@utp.edu.pe>
2026-09-14 17:37:52 -0500

    Se agrego donaciones, terminos y condiciones y se retoco siertos archivos

---
+++ src/app/features/public/legal/TerminosyCondiciones/terminos-condiciones.ts
@@ -0,0 +1,11 @@
+@import({
+selector: 'teminos-condiciones-label',
+	import: [],
+	templateUrl: './terminos-condiciones.html',
+	styleUrl: './terminos-condiciones.scss'
+})
+
+export class terminoscondiciones{
+
+
+}
