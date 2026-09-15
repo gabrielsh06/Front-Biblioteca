cdea19b1e60e7e0285b55e091c0c642d10d8bcac
gabrielsh06 <shgabriel06@gmail.com>
2026-09-14 23:18:22 -0500

    feat: agregar vista home como predefinida y enrutada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { PublicLayout } from './features/public/layouts/public-layout/public-layout';
 import { AdminLayout } from './features/admin/layouts/admin-layout/admin-layout';
+import { Home } from './features/public/pages/home/home';
 
 export const routes: Routes = [
-  { path: '', component: PublicLayout },
+    {
+        path: '', component: PublicLayout,
+        children: [
+            {
+                path: '',
+                component: Home
+            },
+        ]
+    },
   { path: 'admin', component: AdminLayout },
   { path: '**', redirectTo: '' }
 ];
